3ee395d8edbefcb81933f77b9b005a3365028a6b
Dealees <Devin.Arthur.Lees@gmail.com>
2024-07-17 14:10:14 -0700

    Reputation Tiers and ratio to next Tier added
    Added reputation tiers.  The reputation needed to reach the next tier from the current tier is set via an array.
    
    Can get the ratio of progress to the next tier, the reputation contributing to progress toward the next tier, and if currently at the max tier.

--- Assets/_Game/Scripts/Reputation.cs
+++ Assets/_Game/Scripts/Reputation.cs
@@ -6,13 +6,73 @@ public class Reputation : MonoBehaviour
 {
     public int CurrentRep { get; private set; } = 0;
 
+    public int RepTier { get; private set; } = 0;
+
+    public int CurrentTierRep => GetCurrentTierRep();
+
+    public float ratioToNextTier => GetRatioToNextTier();
+
+    public bool atMaxTier => checkAtMaxTier();
+
+    public int[] ReputationPerTier = { 1000, 2000, 3000 };
+
     public void setRep(int rep)
     {
         CurrentRep = rep;
+        RepTier = getTierOfRep(rep);
     }
 
     public void adjustRep(int rep)
     {
         setRep(CurrentRep + rep);
+    }
+
+    private bool checkAtMaxTier()
+    {
+        return RepTier == ReputationPerTier.Length;
+    }
+
+    private int GetCurrentTierRep()
+    {
+        return CurrentRep - getTotalRepOfTier(RepTier);
+    }
+
+    private float GetRatioToNextTier()
+    {
+        if(atMaxTier)
+        {
+            return 1.0f;
+        }
+
+        return (float)(CurrentRep - getTotalRepOfTier(RepTier)) / (float)ReputationPerTier[RepTier];
+    }
+
+    private int getTierOfRep(int rep)
+    {
+        for(int i = 0; i < ReputationPerTier.Length; i++)
+        {
+            if(ReputationPerTier[i] > rep)
+            { 
+                return i;
+            }
+            rep -= ReputationPerTier[i];
+        }
+
+        return ReputationPerTier.Length;
+    }
+
+    private int getTotalRepOfTier(int tier)
+    {
+        if(tier > ReputationPerTier.Length || tier < 0)
+        {
+            throw new System.ArgumentOutOfRangeException($"{tier} is out of reputation tier range.");
+        }
+
+        int rep = 0;
+        for(int i = 0; i < tier; i++)
+        {
+            rep += ReputationPerTier[i];
+        }
+        return rep;
     }
 }
