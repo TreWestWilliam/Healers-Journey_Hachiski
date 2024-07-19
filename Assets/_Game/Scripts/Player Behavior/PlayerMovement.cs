621354e560f81928eb98b3b363f88bb4d482920d
IntrepidNeon <58537791+IntrepidNeon@users.noreply.github.com>
2024-07-19 18:36:45 -0400

    Initial Commit

--- Assets/_Game/Scripts/Player Behavior/PlayerMovement.cs
+++ Assets/_Game/Scripts/Player Behavior/PlayerMovement.cs
@@ -2,61 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float interactDist = 2f;
-    [SerializeField] private CharacterController cc;
-    private Vector3 movement;
-    private Vector3 gravity;
-    [SerializeField] private LayerMask NPCMask;
+	[SerializeField] private float moveSpeed = 5f;
+	[SerializeField] private float sprintMultiplier = 2f;
+	[SerializeField] private float walkMultiplier = 0.5f;
+	[SerializeField] private float interactDist = 2f;
+	[SerializeField] private CharacterController cc;
+	private Vector3 movement;
+	private Vector3 gravity;
+	[SerializeField] private LayerMask NPCMask;
 
-    private void Awake()
-    {
-        gravity = new Vector3 (0, -9.8f, 0);
-    }
+	private void Awake()
+	{
+		gravity = new Vector3(0, -9.8f, 0);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Check for input every frame
-        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	// Update is called once per frame
+	void Update()
+	{
+		// Check for input every frame
+		movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
-        // Normalize movement
-        if(movement.magnitude > 1) movement = movement.normalized;
+		movement.Normalize();
 
-        if(Input.GetKeyDown(KeyCode.E) ) Interact();
-    }
+		movement *= Input.GetKey(KeyCode.LeftShift) ? sprintMultiplier : 1f;
 
-    void FixedUpdate()
-    {
-        // Check if need to move this update
-        if (movement != Vector3.zero)
-        {
-            // Rotate and move
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
-            cc.Move(movement * moveSpeed * Time.fixedDeltaTime);
-        }
+		movement *= Input.GetKey(KeyCode.LeftAlt) ? walkMultiplier : 1f;
 
-        // Apply gravity so slopes work correctly
-        cc.Move(gravity * Time.fixedDeltaTime);
-    }
+		if (Input.GetKeyDown(KeyCode.E)) Interact();
+	}
 
-    private void Interact()
-    {
-        // Check if object in Interactable layer in front of player
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, interactDist, NPCMask))
-        {
-            // Check whether object is NPC or Ingredient using tags
-            if (hit.transform.CompareTag("NPC"))
-            {
-                Debug.Log("NPC: " + hit.transform.name);
-            }
-            else if (hit.transform.CompareTag("Ingredient"))
-            {
-                Debug.Log("Ingredient: " + hit.transform.name);
-            }
-        }
-    }
+	void FixedUpdate()
+	{
+		// Check if need to move this update
+		if (movement != Vector3.zero)
+		{
+			// Rotate and move
+			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+
+			Debug.Log(movement);
+
+			cc.Move(movement * moveSpeed * Time.fixedDeltaTime);
+		}
+
+		// Apply gravity so slopes work correctly
+		cc.Move(gravity * Time.fixedDeltaTime);
+	}
+
+	private void Interact()
+	{
+		// Check if object in Interactable layer in front of player
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, interactDist, NPCMask))
+		{
+			// Check whether object is NPC or Ingredient using tags
+			if (hit.transform.CompareTag("NPC"))
+			{
+				Debug.Log("NPC: " + hit.transform.name);
+			}
+			else if (hit.transform.CompareTag("Ingredient"))
+			{
+				Debug.Log("Ingredient: " + hit.transform.name);
+			}
+		}
+	}
 }
