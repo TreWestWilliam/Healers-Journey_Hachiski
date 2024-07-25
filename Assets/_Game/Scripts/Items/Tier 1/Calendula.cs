cb801d17a891ce00f4c005cfc4bf1430796d4f26
Garrett Slattengren <glslattengren@gmail.com>
2024-07-25 11:29:29 -0700

    Updated inventory slots

--- Assets/_Game/Scripts/Items/Tier 1/Calendula.cs
+++ Assets/_Game/Scripts/Items/Tier 1/Calendula.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlackstrapMolasses : Item
+public class Calendula : Item
 {
     void Awake()
     {
-        itemName = "Blackstrap Molasses";
+        itemName = "Calendula";
     }
 }
