242e1d78704870338ae7a2cd5a3608340b00a9d3
Garrett Slattengren <glslattengren@gmail.com>
2024-07-30 13:58:53 -0700

    Updated collectables
    Updated collectables script to support new interact behavior

--- Assets/_Game/Scripts/Collectable.cs
+++ Assets/_Game/Scripts/Collectable.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collectable : MonoBehaviour
+public class Collectable : MonoBehaviour, IInteractable
 {
     [SerializeField] private int numIngredients;
     [SerializeField] private string itemName;
     [SerializeField] private GameObject self;
     public GameObject invManager;
 
-    public void Collect()
+    public void Engage()
     {
         InventoryManager inv = invManager.GetComponent<InventoryManager>();
         inv.AddItem(itemName, numIngredients);
         self.SetActive(false);
     }
 
-
+    public void Disengage()
+    {
+        return;
+    }
 }
